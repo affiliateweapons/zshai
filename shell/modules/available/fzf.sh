c089b3de88549620414be7d16325dd1e22c26124
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2021-11-15 17:19:28 +0700

    fzf preview file

--- shell/modules/available/fzf.sh
+++ shell/modules/available/fzf.sh
@@ -4,3 +4,15 @@ alias mfz="man fzf"
 fzp() {
   FZF_DEFAULT_OPTS='--preview="f={}; [[ -f $f ]] && fold -w 120 -s  {} " --preview-window=top,20 --height 100%' 
 }
+
+fzf_preview() {
+
+  input=$(cat /dev/stdin)
+
+  [[ ! -z $input ]] && {
+    echo $input | fzf --preview="cat {}"
+  } || {
+    echo "No results";
+  }
+
+}
