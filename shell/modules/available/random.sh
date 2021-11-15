8f8228905a1a77c4c0b77c2f7a7d1232c9f061c1
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2021-11-15 17:17:11 +0700

    random quote function

--- shell/modules/available/random.sh
+++ shell/modules/available/random.sh
@@ -7,3 +7,19 @@ random_name() {
 alias rn="random_name"
 alias sfx="cat $ZSHAI/list/suffix"
 alias pfx="cat $ZSHAI/list/prefix"
+
+
+random_quote() {
+
+  repeat 3; echo 
+  local quote="$(random_from_list quote)"
+  echo $quote
+  repeat 3;echo
+}
+
+random_from_list() {
+  local count="${2:-1}"
+  local list="${1}"
+  local item="$(shuf -n $count $ZSHAI_DATA/list/$list)"
+  echo $item
+}
