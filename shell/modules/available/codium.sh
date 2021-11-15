c17b8cc8e54cf20d6bb7c073b33b7ba247f2e9ae
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2021-11-16 04:03:26 +0700

    update codium

--- shell/modules/available/codium.sh
+++ shell/modules/available/codium.sh
@@ -1,2 +1,2 @@
 # run codium from an appimage
-alias codium="~/Downloads/apimz/VSCodium-1.62.0-1636195471.glibc2.17-x86_64.AppImage &"
+alias codium="~/Downloads/apimz/VSCodium-1.62.2-1636676593.glibc2.17-x86_64.AppImage &"
