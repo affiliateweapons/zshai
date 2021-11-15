d0a562b85b563954af001559c147d308dabfef4e
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2021-11-15 17:16:40 +0700

    updated some docker aliases and functions

--- shell/modules/available/docker.sh
+++ shell/modules/available/docker.sh
@@ -12,16 +12,16 @@ alias di="docker image"
 alias dis="docker inspect"
 alias dcl="docker container ls -a"
 alias dlc="docker container ls -a"
-alias dli="docker ps -a"
+alias dli="docker images -a"
 alias dps="docker ps -a"
 alias d="docker"
 alias dx="docker exec -it"
-alias dxr="docker exec -it -rm"
+alias dxr="docker --rm exec -it"
 alias dpra="docker prune -a"
-alias db="docker build -t"
+alias db="docker_build"
 alias dp="docker pull"
 alias dks="docker stop"
-
+#alias drum="docker run --rm"
 # docker-compose
 alias dcu="docker-compose up"
 alias dcd="docker-compose down"
@@ -40,7 +40,6 @@ dpl() {
   docker pull  $version
 }
 
-
 # inspect images
 docker_inspector() {
 local  docker_images=$(docker images -qa)
@@ -48,3 +47,25 @@ echo $images
 #  sudo docker inspect $(docker images -qa)  | gron  | fzf -e -i --ansi --no-sort
 }
 alias dis="docker_inspector"
+
+
+docker_build() {
+  [[ -z $DOCKER_IMAGE_NAMESPACE ]] && \
+    docker build -t $1 . \
+  || docker build -t $DOCKER_IMAGE_NAMESPACE/$1 .
+}
+
+dockerfile() {
+echo "New Dockerfile"
+ls $ZSHAI/containers/dockerfiles;
+}
+
+docker_volume_create() {
+  docker volume create $1
+}
+alias dvc="docker_volume_create"
+
+docker_run_rm() {
+docker run --rm $@
+}
+alias drum="docker_run_rm"
