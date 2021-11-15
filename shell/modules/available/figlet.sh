5ba082ded8827147f000e59414272e7f75e9b94c
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2021-11-15 17:13:39 +0700

    add functions to run figlet in docker

---
+++ shell/modules/available/figlet.sh
@@ -0,0 +1,27 @@
+#alias figlet='docker run -i --rm zshai/figlet'
+
+figlet() {
+local font="$1"
+[[ -z $2 ]] && {
+    drum  -it -v figlet_data:/data figlet -f "/data/figlet-fonts/3d.flf" $@
+} || {
+    shift
+    drum  -it -v figlet_data:/data figlet -f "/data/figlet-fonts/$font.flf" $@
+  }
+}
+
+
+#@todo cache results
+fzfiglet() {
+  local default_text="Powered by Zsh.Ai"
+  local text="${1:-$default_text}"
+  local fonts_dir=$ZSHAI_DATA/figlet/fonts/available
+  local fonts=$('ls' $fonts_dir)
+
+  #  echo $fonts | fzf 
+  for i in $(echo $fonts)
+  do
+    local file=$fonts_dir/$i
+    drum  -it -v $(readlink -f $file):/tmp/figlet_$file figlet -f /tmp/figlet_$file $text
+  done
+}
--- shell/modules/available/list.sh
+++ shell/modules/available/list.sh
@@ -1,22 +1,46 @@
 #!/usr/bin/env zsh
 list() {
-  local action=${1:-all}
+
+  list::modules() {
+    echo "options: $1"
+    case $1 in
+      enabled)
+        find -type f -type l  $ZSHAI_MODULES_DIR/enabled | fzf_preview
+        ;;
+      available)
+        find  -type f -type l $ZSHAI_MODULES_DIR/available | fzf_preview
+        ;;
+      *)
+        find  $ZSHAI_MODULES_DIR --recursive
+        ;;
+      esac
+  }
+
+  list::new() {
+    #local name="$1"
+    #vared -p "Name: " -e name
+    nano ~/.zshai/list/$1
+  }
+
+
+
+  local action="${1:-all}"
+
+  echo "action: $action"
+
+
   case $1 in
+  modules)
+    shift
+    list::modules $@
+    ;;
   new)
-    list::new
+    list::new $@
     shift
     ;;
   *)
-    list::new $1
+    list::new $@
     ;;
   esac
-
-  list::new() {
-    local name="$1"
-
-    
-    #vared -p "Name: " -e name
-    nano ~/.zshai/list/$name
-  }
 
 }
