37ca7cbf1b63b0d201995e9a3be497e9d9d7f976
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2021-11-16 08:31:58 +0700

    add server nginx

--- shell/modules/available/nginx.sh
+++ shell/modules/available/nginx.sh
@@ -1,2 +1,19 @@
 alias snr="sudo service nginx restart"
 alias cdngx="cd /usr/share/nginx/;ls"
+
+
+addnginx() {
+  domain="$1"
+  [[ -z "$domain" ]] && echo "No domain specified" && return
+
+  local webroot="/usr/share/nginx"
+  local config="/etc/nginx/conf.d"
+  local skel="$ZSHAI/lib/skel/nginx/server.conf"
+  
+  cp ./server.conf /etc/nginx/conf.d/$domain.conf \
+  && mkdir $webroot/$domain \
+  && cat $skel | sed 's/{DOMAIN}/$domain/g' > $config \
+  && sudo service nginx restart \
+  && cd /usr/share/nginx/$domain
+  
+}
