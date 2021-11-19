f813a8d1244242bdd33c22c843b0da928653be6e
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2021-11-19 14:46:41 +0700

    find ips in auth log

--- shell/modules/available/apt.sh
+++ shell/modules/available/apt.sh
@@ -1,1 +1,10 @@
 alias sai="sudo apt install"
+
+apt_upgrade() {
+  local upgrades=$(mktemp /tmp/upgrades.XXXXX)
+  apt list --upgradeable > $upgrades
+  for i in $(cat $upgrades | sed 's/\/.*//g'); apt-get install $i -y 
+
+  rm "$upgrades"
+
+}
--- shell/modules/available/auth.sh
+++ shell/modules/available/auth.sh
@@ -1,2 +1,13 @@
 alias auth1k="tail -1000 /var/log/auth.log"
 alias auth5k="tail -5000 /var/log/auth.log"
+
+
+auth_ips() {
+  cat /var/log/auth.log | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}\b"  | grep -v "$(cat $ZSHAI_DATA/exclusion/local-ips)"
+}
+
+
+ipgrep() {
+  local text=$(cat /dev/stdin)
+  echo $text | grep -E $1 "([0-9]{1,3}\.){3}[0-9]{1,3}\b" 
+}
