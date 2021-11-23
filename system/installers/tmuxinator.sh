4b43b3f3dff99d1a19d14b249a1d8be0bb332025
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2021-11-23 09:54:07 +0700

    add tmuxinator installer

--- system/installers/tmuxinator.sh
+++ system/installers/tmuxinator.sh
@@ -1,1 +1,2 @@
 sudo apt install tmux tmuxinatyor -y
+wget https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.zsh -O /usr/local/share/zsh/site-functions/_tmuxinator
