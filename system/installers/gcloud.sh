0a89256628272f04a5bbc78b8c8a086c672f88ea
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2021-12-02 15:59:46 +0700

    add gcloud

--- system/installers/gcloud.sh
+++ system/installers/gcloud.sh
@@ -1,3 +1,4 @@
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-365.0.1-linux-x86_64.tar.gz
 tar  xzvf google-cloud-sdk-365.0.1-linux-x86_64.tar.gz 
 ./google-cloud-sdk/install.sh
+
