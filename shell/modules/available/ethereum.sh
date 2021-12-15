087e430107e3bc6f6ae4b39ccd41e6afdc6ad345
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2021-12-15 17:31:06 +0700

    add ethereum

---
+++ shell/modules/available/ethereum.sh
@@ -0,0 +1,8 @@
+install_ethereum() {
+  sudo apt-get install software-properties-common
+  sudo add-apt-repository -y ppa:ethereum/ethereum
+  sudo apt-get update
+  sudo apt-get install ethereum
+}
+
+
