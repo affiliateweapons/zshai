6c917817d4c6363e0799c63ca36159b3345d72b8
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2022-01-14 14:07:48 +0700

    make ramdisk interactive

--- shell/modules/available/ramdisk.sh
+++ shell/modules/available/ramdisk.sh
@@ -1,29 +1,50 @@
 ramdisk() {
 
-  CLS="ramdisk"
-  local size_gb="${1:-2}"
-  local disk_name="ramdisk"
-  local mount="/mnt/ramdisk"
+  local CLS="ramdisk"
 
-
-  $CLS::setup() {
-    shift
-    local size_gb="${1:-2}"
-    local mount="/mnt/ramdisk"
-    local disk_name="ramdisk"
-    let size=2048
-    size=$(( size=size_gb*1024 ))
-    echo "setup ramdisk of:  $size m"
-
-    [[ ! -e "$mount" ]] && {
-      mkdir -p "$mount"
-      echo "created folder: $mount"
-    }
-
-    sudo mount -t tmpfs -o size="$size"m  "$disk_name"  "$mount"   
+  ramdisk::list() {
+    ramdisk::default
+  }
+  $CLS::default() {
+    cd "$mount_folder"
+    mount | grep ram
+    ls
   }
 
-  $CLS::speedtest() {
+
+  ramdisk::help() {
+    echo $(cat<<EOF
+Usage: todo [command] [subcommand]
+  Current datadir:
+    $current
+  Available commands:
+    setup
+    speedtest
+EOF
+)
+
+  }
+
+  ramdisk::setup() {
+    ramsize="$1"
+
+    vared -p "Choose RAM size (gb): " -e ramsize
+    local mountfolder="/mnt/ramdisk"
+    local disk_name="ramdisk"
+    let size="$(( ramsize * 1024 ))"
+    echo "setup ramdisk of:  $size m"
+
+
+    
+    vared -p "Mount folder: " -e mountfolder
+    [[ ! -e "$mountfolder" ]] && {
+      sudo mkdir -p "$mountfolder"
+      echo "created folder: $mountfolder"
+    }
+    sudo mount -t tmpfs -o size="$size"m  "$disk_name"  "$mountfolder"
+  }
+
+  ramdisk::speedtest() {
     local target="/mnt/ramdisk/zero"
     sudo dd if=/dev/zero of="$target" bs=4k count=100000
   }
