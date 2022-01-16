04c14e161b55013fea18f2bd37c88cfd54458e6f
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2022-01-16 20:46:04 +0700

    update function cdb

--- shell/modules/available/cdb.sh
+++ shell/modules/available/cdb.sh
@@ -4,15 +4,41 @@ cdb() {
 
   $CLS::query() {
     local param1="$1"
-    curl -X POST -H "Content-Type: application/json" \
-      $($CLS connect) -d @$param1
+    shift
+
+    [[ -f "$param1" ]] && {
+      $CLS connect "-d @$param1"
+    } || {
+      $CLS connect
+    }
+  }
+  $CLS::get() {
+    CDB_REQUEST_METHOD="GET"
+    $CLS connect $@
   }
   $CLS::default() {
     # $CLS::query $@
+    echo $1
   }
   $CLS::connect() {
-    local SERVER_URL="${COUCHDB_URL:-1}"
-    echo $SERVER_URL
+    local CDB_ENDPOINT="${CDB_ENDPOINT:-$1}"
+    local CBD_DATA="${2}"
+ #    local CDB_SERVER="${CDB_SERVER:-$2}"
+ #   local CDB_DATABASE="$${CDB_DATABASE:-$3}"
+    local CDB_BUILD_URL="$CDB_SERVER/$CDB_DATABASE$CDB_ENDPOINT"
+    local CDB_CONNECT_URL="${CDB_CONNECT_URL/:-$CDB_BUILD_URL}/$CDB_ENDPOINT"
+    local CDB_REQUEST_METHOD="${CDB_REQUEST_METHOD:-POST}"
+    echo "Connecting to: $CDB_CONNECT_URL"
+    echo "Request method: $CDB_REQUEST_METHOD"
+    echo "Endpoint: $CDB_ENDPOINT"
+    echo "Params: $2"
+    local api_request="$(echo curl -X \"$CDB_REQUEST_METHOD\" -H \"Content-Type: application/json\" \"$CDB_CONNECT_URL\" $CDB_DATA)"
+    [[ ! -z "CDB_VERBOSE" ]] && echo "$api_request"
+
+    eval $api_request
+  }
+
+  $CLS::cleanup() {
     unset $SERVER_URL
   }
 
