502a96028f055252b585e9be86a46289818717f1
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2022-01-19 04:51:10 +0700

    update function docker

--- shell/modules/available/docker.sh
+++ shell/modules/available/docker.sh
@@ -1,15 +1,15 @@
 export FORMAT_FULL="table{{.ID}}\t{{.Image}}\t{{.Command}}\t{{.Status}}\t{{.Size}}"
+
 alias docs="docker search"
 alias drunrm="docker run --network host --rm -it"
 didep() {
-  [[ -z "$1" ]] && return 
+  [[ -z "$1" ]] && return
   for i in $(docker images -q)
   do
       docker history $i | grep -q $1 && {
           docker images | grep  $i
-      } 
+      }
   done | sort -u
-  
 }
 docker_stop() {
   systemctl {stop,mask} docker.socket
@@ -18,9 +18,10 @@ docker_stop() {
   systemctl {stop,disable,mask} docker.{socket,service}
 }
 docker_enable() {
-systemctl enable docker{.socket,.service}
-systemctl start docker{.socket,.service}
-
+  systemctl enable docker{.socket,.service}
+  systemctl start docker{.socket,.service}
+  systemctl enable containerd.service
+  systemctl start containerd.service
 }
 # docker aliaes anad functions
 rlc() {
@@ -40,11 +41,11 @@ drmf() {
   sudo docker rm -f $1
 #  dcl
 }
+
 # docker
 build() {
-[[ -d "build" ]] && cd build && return 
-
-[[ -f "build.sh" ]] &&  . ./build.sh
+  [[ -d "build" ]] && cd build && return
+  [[ -f "build.sh" ]] &&  . ./build.sh
 }
 #alias build=". ./build.sh"
 alias run=". ./run.sh"
@@ -99,53 +100,50 @@ alias pu="docker start portainer"
 
 
 docker_rm() {
-docker stop $1
-docker rm  $1
-
-echo "$1 has been removed"
+  docker stop $1
+  docker rm  $1
+  echo "$1 has been removed"
 }
 
 # docker pull ${image}latest
 dpl() {
-  req1 $1 && return 
+  req1 $1 && return
   local version=$1':latest'
   docker pull  $version
 }
 
 # inspect images
 docker_inspector() {
-local  docker_images=$(docker images -qa)
-echo $docker_images
-#  sudo docker inspect $(docker images -qa)  | gron  | fzf -e -i --ansi --no-sort
+  sudo ls 2>/dev/null
+  local  docker_images=$(docker images -qa)
+  # echo $docker_images
+  sudo docker inspect $($docker_images)  | gron  | fzf -e -i --ansi --no-sort
 }
 alias dis="docker_inspector"
 
-
 docker_build() {
-local name="$1"
-
-vared -p "container name: $1 "  -e name
-
+  local name="$1"
+  vared -p "container name: $1 "  -e name
   [[ -z $DOCKER_IMAGE_NAMESPACE ]] && \
     docker build -t $name  . $DOCKER_OPTS \
   || docker build -t $DOCKER_IMAGE_NAMESPACE/$name .
 }
 
 dockerfile() {
-echo "New Dockerfile"
-ls $ZSHAI/containers/dockerfiles;
+  echo "New Dockerfile"
+  ls $ZSHAI/containers/dockerfiles;
 }
-
-
 
 docker_volume_create() {
   docker volume create $1
 }
+
 alias dvc="docker_volume_create"
 
 docker_run_rm() {
-docker run --rm $@
+  docker run --rm $@
 }
+
 alias drum="docker_run_rm"
 
 dockerbash() {
@@ -164,20 +162,26 @@ alias dsh="dockersh"
 
 
 docker_format() {
-docker container ls --format='{{json .}}' | jq
+  docker container ls --format='{{json .}}' | jq
 }
+
 docker_containers_json() {
-  docker ps -a --format "{{.ID}}\t{{.Names}}\t{{.Image}}" | column -t -J --table-columns "id,names,image"  
+  docker ps -a --format "{{.ID}}\t{{.Names}}\t{{.Image}}" | column -t -J --table-columns "id,names,image"
 }
-export_image_inspect(){ 
-  for i in $(docker images -a -q);docker inspect $i > /tmp/inspect$i.json 
+
+export_image_inspect(){
+  for i in $(docker images -a -q);docker inspect $i > /tmp/inspect$i.json
 
   cd /tmp/inspect
-  cat $('ls') > all.json 
+  cat $('ls') > all.json
 
-# list all layers
-#cat $('ls') | gron | grep Layers
+  # list all layers
+  #cat $('ls') | gron | grep Layers
 }
 list_overlay_hashes() {
-  sudo find /var/lib/docker/overlay2 -maxdepth 1 -type d  
+  sudo find /var/lib/docker/overlay2 -maxdepth 1 -type d
+}
+
+dxz() {
+  docker exec -it $1 /bin/zsh $2
 }
