41e32ea88d0a40ee1755d8058507e52e6fd71d7d
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2022-01-20 18:03:26 +0700

    ps functions

--- shell/modules/available/ps.sh
+++ shell/modules/available/ps.sh
@@ -1,12 +1,16 @@
-ps () {
-	command ps -ax | grep -v '\[kworker' | grep --color=auto -v '0:00 \['
+psx () {
+	command ps -ax | grep -v '\[kworker' | grep --color=auto -v '0:00 \[' \
+  | grep -v "/usr/libexec" | grep -v "brave"
 }
+ps1 () { ps fe }
 
+psf() { command ps f }
 
+pse() { ps e }
 
 ps2() {
-  [[ -z $1 ]] && \
-$commands[ps]  ah  || 
-$commands[ps] $@
-
+  [[ -z $1 ]] \
+  && command [ps] ah \
+  || command [ps] $@
 }
+
