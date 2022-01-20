abd67ea392433e49586510322f2d87b85bf45820
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2022-01-21 05:15:47 +0700

    update function prepend

--- shell/modules/available/prepend.sh
+++ shell/modules/available/prepend.sh
@@ -2,10 +2,17 @@ prepend() {
 
   local text=( $(cat /dev/stdin) )
   local prepend="$1"
+  local prepend_text="$1"
 
   for i in $(seq $#text)
   do
-    [[ "$prepend" = "numbered" ]] &&  prepend_text="$i " || prepend_text="$prepend"
+    [[ "$prepend" = "numbered" ]] &&  {
+      prepend_text="$i " || prepend_text="$prepend"
+    }
+    [[ "$prepend" = "linecount" ]] &&  {
+      prepend_text="$(a=$text[$i];cat  ${a/*\ } | wc -l) " || prepend_text="$prepend"
+    }
+
     echo $prepend_text"$text[$i]"
   done
 }
