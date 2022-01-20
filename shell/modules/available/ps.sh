a3379adf2b4e50ded83ee9a676e8e1173a51546b
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2022-01-20 18:33:53 +0700

    update function ps

--- shell/modules/available/ps.sh
+++ shell/modules/available/ps.sh
@@ -1,7 +1,16 @@
 psx () {
-	command ps -ax | grep -v '\[kworker' | grep --color=auto -v '0:00 \[' \
-  | grep -v "/usr/libexec" | grep -v "brave"
+  command ps -ax | grep -v '\[kworker' | grep --color=auto -v '0:00 \[' \
+  | grep -v "/usr/libexec" \
+  | grep -v "brave" \
+  | grep -v "/lib/systemd/systemd" \
+  | grep -v "/usr/sbin/chronyd" \
+  | grep -v "/usr/sbin/rsyslogd" \
+  | grep -v "/usr/bin/google_guest_agent" \
+  | grep -v "/usr/bin/google_osconfig_agent" \
+  | grep -v "/sbin/agetty"
 }
+
+
 ps1 () { ps fe }
 
 psf() { command ps f }
