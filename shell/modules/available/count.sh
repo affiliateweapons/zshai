daa2891101b38e725f5e4abd19d56628faf444a6
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2022-01-21 13:24:00 +0700

    update function count

--- shell/modules/available/count.sh
+++ shell/modules/available/count.sh
@@ -1,4 +1,3 @@
-# count lines in a file
 count() {
   [[ -f /var/log/$1.log ]] && {
     cat /var/log/$1.log | wc -l
@@ -6,7 +5,45 @@ count() {
     [[ -f $1 ]]  &&  {
       cat $1 | wc -l
     } || {
-      echo "file $1 does not exist"
+
+      data=$(cat /dev/stdin) 
+echo $data | wc -l
+#      echo "file $1 does not exist"
     }
   }
 }
+
+
+list_count_words() {
+local ignorefile="$ZSHAI_DATA/wordlist/stopwords"
+local input="$(cat /dev/stdin)"
+echo "${input:l}" \
+| strip_ansi \
+| sed 's,\t,,g' \
+| sed 's,[/-], ,g' \
+| grep -v "^\s*$" \
+| sed 's/\b\w\b \?//g' \
+| tr '[:space:]' '[\n*]' \
+| sed '/^[[:space:]]*$/d' \
+| grep -v "[^a-z]" \
+| sort \
+| uniq -c \
+| sort -bnr \
+| grep -vwf $ZSHAI_DATA/wordlist/stopwords \
+#tr '[:space:]' '[\n*]' \
+#| grep -v "[^a-z]" \
+#| sed 's/\b\w\b \?//g' \
+#| sed '/^[[:space:]]*$/d' \
+#| sort \
+#| uniq -c \
+#| sort -bnr \
+#| grep -vF "$ZSHAI_DATA/wordlist/stopwords" \
+#| tr '[:space:]' '[\n*]' \
+#| grep -v "abc"
+
+}
+
+#| grep -vF "$ZSHAI_DATA/wordlist/stopwords" \
+#| grep -vF "$ZSHAI_DATA/wordlist/xac" \
+#| grep -vF "$ZSHAI_DATA/wordlist/xaa" \
+#| grep -vF "$ignorefile" \
