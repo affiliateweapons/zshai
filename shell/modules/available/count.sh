cacf157f553eacb675b8ead3b2d6e87e41e57c83
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2022-01-21 13:25:42 +0700

    update function count

--- shell/modules/available/count.sh
+++ shell/modules/available/count.sh
@@ -7,43 +7,10 @@ count() {
     } || {
 
       data=$(cat /dev/stdin) 
-echo $data | wc -l
-#      echo "file $1 does not exist"
+      echo $data | wc -l
+      #echo "file $1 does not exist"
     }
   }
 }
 
 
-list_count_words() {
-local ignorefile="$ZSHAI_DATA/wordlist/stopwords"
-local input="$(cat /dev/stdin)"
-echo "${input:l}" \
-| strip_ansi \
-| sed 's,\t,,g' \
-| sed 's,[/-], ,g' \
-| grep -v "^\s*$" \
-| sed 's/\b\w\b \?//g' \
-| tr '[:space:]' '[\n*]' \
-| sed '/^[[:space:]]*$/d' \
-| grep -v "[^a-z]" \
-| sort \
-| uniq -c \
-| sort -bnr \
-| grep -vwf $ZSHAI_DATA/wordlist/stopwords \
-#tr '[:space:]' '[\n*]' \
-#| grep -v "[^a-z]" \
-#| sed 's/\b\w\b \?//g' \
-#| sed '/^[[:space:]]*$/d' \
-#| sort \
-#| uniq -c \
-#| sort -bnr \
-#| grep -vF "$ZSHAI_DATA/wordlist/stopwords" \
-#| tr '[:space:]' '[\n*]' \
-#| grep -v "abc"
-
-}
-
-#| grep -vF "$ZSHAI_DATA/wordlist/stopwords" \
-#| grep -vF "$ZSHAI_DATA/wordlist/xac" \
-#| grep -vF "$ZSHAI_DATA/wordlist/xaa" \
-#| grep -vF "$ignorefile" \
