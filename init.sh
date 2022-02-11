7023495030ca5e1505902be0094db1707bd838b4
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2022-02-12 06:33:02 +0700

    update init.sh

--- init.sh
+++ init.sh
@@ -1,18 +1,53 @@
 #!/usr/bin/env zsh
-ZSHAI=~/zshai
-ZSHAI_DATA=~/.zshai
+typeset -g ZSHAI="${HOME}/zshai" 
+typeset -g ZSHAI_DATA="${HOME}/.zshai"
+typeset -g LS_CMD="$commands[ls]"
+typeset -Ag ZSHAI_DIRS=()
+
+typeset -ag enabled=()
+
+
+zdir() {
+  local q="${1:-BASE}"
+  ZSHAI_DIRS=(
+    [base]="$ZSHAI"
+    [user]="$ZSHAI_DATA"
+    [shell]="$ZSHAI/shell"
+    [plugins]="$ZSHAI/plugins"
+    [modules]="$ZSHAI/shell/modules"
+    [completion]="$ZSHAI/shell/completion"
+    [alias]="$ZSHAI/shell/alias"
+  )
+
+  echo "$ZSHAI_DIRS[${q:l}]"
+}
+
+enabled=(
+"$(cat $(zdir user)/enabled)"
+)
+
+
 # this the entry file that will be sourced by .zshrc
-eval $(cat $ZSHAI/.zshai.env)
-export ZSHAI_LOADED_COMPLETIONS=()
+eval $(cat "$ZSHAI/.zshai.env")
+typeset -g ZSHAI_LOADED_COMPLETIONS=(doctl)
+
 # load core aliases and functions
-'source' $ZSHAI/core.sh
+'source' "$ZSHAI/core.sh"
 
 # looad local specific aliases
-[ -f $ZSHAI_DATA/aliases.sh ] && 'source' $ZSHAI_DATA/aliases.sh
+[[ -f "$ZSHAI_DATA/aliases.sh" ]] && 'source' "$ZSHAI_DATA/aliases.sh"
 
 # load completion
-for i in $('ls' $ZSHAI/shell/completion);'source' $ZSHAI/shell/completion/$i; 
+((enabled[(I)LOAD_COMPLETION_FUNCTIONS])) && {
+  for i in $("$LS_CMD" "$(zdir completion)"); {
+    'source' "$(zdir completion)/$i"
+  }
+}
 
+# load plugins
+# first  load the module
+load_module plugins
+plugins load
 
 # mark loaded
-export ZSHAI_LOAD_COMPLETED=true
+typeset -g ZSHAI_LOAD_COMPLETED=true
