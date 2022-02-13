365f356201b77d513d2f00e1eeb085ee427dfdf2
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2022-02-14 02:17:52 +0700

    added vultr-cli module

--- shell/modules/available/vultr.sh
+++ shell/modules/available/vultr.sh
@@ -1,16 +1,55 @@
-export VULTR_DIR=~/.zshai/vultr
 
-vul() {
-  apikey=$(decrypt $ZSHAI_DATA/gpg/apikeys/vultr.gpg)
-  sudo docker run -ti --rm --env=VULTR_API_KEY="$apikey" vultr-cli $@
-}
+vultr() {
+  CLS="vultr"
+  typeset -g VULTR_DIR="$HOME/.zshai/vultr"
 
-vil() {
-  local cache=${VULTR_DIR}/instance-list
+  ORIGINAL_FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"
+  export FZF_DEFAULT_OPTS="$FZF_THEME_OPTS$(cat<<EOF
 
-  [[ -e ${cache} && -z $1 ]] && cat ${cache} || {
-    mkdir -p ${VULTR_DIR}
-    touch ${cache}
-    vul instance list > ${cache}
+EOF
+)"
+
+
+  $CLS::list() {
+    local cache="${VULTR_DIR}/instance-list"
+
+    [[ -e "${cache}" && -z $1 ]] && cat "${cache}" || {
+      mkdir -p "${VULTR_DIR}"
+      touch "${cache}"
+      vul instance list > ${cache}
+    }
   }
+
+  $CLS::default() {
+    [[ "$1" = "-c" ]] && VULTR_CLI_CACHE_DISABLED=true && shift
+    [[ "$1" = "+c" ]] && unset VULTR_CLI_CACHE_DISABLED && shift
+    [[ -z "$VULTR_CLI_CACHE_DISABLED" ]] && $CLS::_cache $@ || $CLS::_cmd $@
+  }
+
+  $CLS::_cache() {
+    local md5="$(echo $@ | md5)"
+    local cache_file="$(:dir cache)/$md5"
+    [[ -f "$cache_file" ]] && {
+      :info "${RED}HOT CACHE" "$cache_file"
+      cat "$cache_file"
+     } || {
+      :info "${BLUE}COLD CACHE${RESET}" ""
+      local response="$(vultr::_cmd $@)"
+      echo "$response" > "$cache_file"
+      cat "$cache_file"
+    }
+  }
+
+  $CLS::_cmd() {
+    apikey=$(decrypt $ZSHAI_DATA/gpg/apikeys/vultr.gpg)
+    'docker' run -ti --rm --env=VULTR_API_KEY="$apikey" vultr-cli $@
+  }
+
+  $CLS::_cleanup() {
+    FZF_DEFAULT_OPTS="$ORIGINAL_FZF_DEFAULT_OPTS"
+  }
+
+  subcommands $CLS $@  
+
 }
+zshai_alias 'vul="vultr"'
