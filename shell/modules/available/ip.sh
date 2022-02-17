2321a6909e8214dc72a2176c3f95a02a66d014c8
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2022-02-17 08:00:48 +0700

    add ip functions

--- shell/modules/available/ip.sh
+++ shell/modules/available/ip.sh
@@ -1,8 +1,15 @@
 alias ipf="ip a"
 alias ip="ip a | grep global"
 
-
-
 ips(){
-grep -a -E "([0-9]{1,3}\.){3}[0-9]{1,3}\b" $@
+  grep -a -E "([0-9]{1,3}\.){3}[0-9]{1,3}\b" $@
+}
+
+ipthis() {
+  #  curl -s https://ip2.sh
+  curl icanhazip.com
+}
+
+myip() {
+  curl icanhazip.com
 }
