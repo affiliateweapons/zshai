d1c8d14bd8c76a717615263a9c930ae290773425
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2022-02-19 01:31:29 +0700

    discord selfbot scraper

---
+++ shell/modules/scraper/discord.sh
@@ -0,0 +1,28 @@
+scraper-discord() {
+  CLS="scrape-discord"
+
+  $CLS::setup() {
+    apt install python3
+    ln -s /usr/bin/python3 /usr/bin/python
+    apt install python3.9-venv
+  }
+
+  $CLS::venv() {
+    local name="${1:-venv}"
+    python -m venv "$name"
+    source "$name"/bin/activate
+    python -m pip install discum
+  }
+
+  $CLS::default() {
+    local script="${1:-scraper.py}"
+    python "$script"
+  }
+
+  $CLS::off(){
+    deactivate
+  }
+
+  subcommands $CLS $@
+
+}
