4d63666eb16b5750a3ca700e025b2d698d126a5a
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2022-02-24 09:18:42 +0700

    add zrip

--- shell/modules/available/wget.sh
+++ shell/modules/available/wget.sh
@@ -1,11 +1,36 @@
+zrip() {
+  local domain="$(echo $1 | extract_domain)"
+  vared -p "Save as: " -e domain
+
+  rwget $1 $domain
+  #[[ ! -z "$2" ]] && tardir $2
+  tardir "$domain"
+  ls "$domain"
+}
+
+rwget() {
+
+  local url="${1}"
+  local target="${2:-lander}"
+  :info "Current PWD" "$PWD"
+
+  [[  ! -d "$target" ]] && mkdir -p "$target"
+  wget \
+  -e robots=off -nd \
+  -k -p -H -E -K \
+  --no-parent \
+  --no-check-certificate \
+  --no-verbose \
+  --progress=bar:force:noscroll \
+  "$url" \
+  -P "$target"
+
+}
 
 download_list() {
-local dir="$PWD/build"
-for q in $(cat $1);
-do
-wget ${q/*=/} -O $dir/${q/=*/}.png
-
-done
+  local dir="$PWD/build"
+  for q in $(cat $1);
+  do
+    wget ${q/*=/} -O $dir/${q/=*/}.png
+  done
 }
-# echo ${q/*=/}
-#echo ${q/=*/}
