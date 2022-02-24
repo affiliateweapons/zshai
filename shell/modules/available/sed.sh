81e6fda6adb0fb34c801f8831b64f25f71820565
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2022-02-24 09:17:39 +0700

    add multiple functions

--- shell/modules/available/sed.sh
+++ shell/modules/available/sed.sh
@@ -1,14 +1,27 @@
 remove_quotes() {
-
-cat /dev/stdin | sed 's/"//g'
+  cat /dev/stdin | sed 's/"//g'
 }
 
-
 remove_single_quotes() {
-cat /dev/stdin | sed "s/'//g"
+  cat /dev/stdin | sed "s/'//g"
 }
 
 remove_all_quotes() {
-cat /dev/stdin | sed "s/'//g" | sed 's/"//g'
+  cat /dev/stdin | sed "s/'//g" | sed 's/"//g'
+}
 
+surround_keys_brackets() {
+  sed 's,\(.*\)=,[\1]=,'
+}
+
+remove_ansi() {
+  sed 's/\[^[[0-9;]*[a-zA-Z]//gi'
+}
+
+trim() {
+  sed 's/  */ /g'
+}
+
+extract_domain() {
+  sed -e 's/[^/]*\/\/\([^@]*@\)\?\([^:/]*\).*/\2/'
 }
